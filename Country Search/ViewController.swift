c1b0b9acefe9bb11f8161f276740c8d413e51d7f
Andre Hoffmann <dev@effzehn.de>
2019-05-17 16:28:43 +0200

    Initial Commit

---
+++ Country Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Country Search
+//
+//  Created by A. Hoffmann on 17.05.19.
+//  Copyright © 2019 A. Hoffmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
